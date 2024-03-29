0c5f0aee92d38eb6cf115f06c8781481e37360cb
chenjia <chenjia.go@qq.com>
2024-03-29 11:05:50 +0800

    update

--- starter/meger_starter/main.go
+++ starter/meger_starter/main.go
@@ -15,14 +15,14 @@ import (
 )
 
 func init() {
-	//db.MustInitMySQL("sc_kupu:Sc_kupu_1234@tcp(10.128.0.28:3306)/qiyee_job_data") //生产环境
-	db.MustInitMySQL("kp_user_local:Kupu123!@#@tcp(10.131.0.206:3306)/qiyee_job_data") //预发环境
+	db.MustInitMySQL("sc_kupu:Sc_kupu_1234@tcp(10.128.0.28:3306)/qiyee_job_data") //生产环境
+	//db.MustInitMySQL("kp_user_local:Kupu123!@#@tcp(10.131.0.206:3306)/qiyee_job_data") //预发环境
 	cache.InitKeyCache()
 }
 
-// var brokers = []string{"10.128.0.94:9092", "10.128.0.156:9092", "10.128.0.124:9092"} // 生产环境
-var brokers = []string{"10.128.0.94:9092", "10.128.0.156:9092", "10.128.0.124:9092"} // 预发环境
-var topic = "go-profile-merge"                                                       // 要消费的主题
+var brokers = []string{"10.128.0.94:9092", "10.128.0.156:9092", "10.128.0.124:9092"} // 生产环境
+// var brokers = []string{"10.128.0.94:9092", "10.128.0.156:9092", "10.128.0.124:9092"} // 预发环境
+var topic = "go-profile-merge" // 要消费的主题
 var consumerGroup = "gemini-merge-group"
 
 func main() {
@@ -78,7 +78,7 @@ func (ConsumerGroupHandler) Cleanup(sarama.ConsumerGroupSession) error { return 
 func (h *ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
 		if h.ProcessMessage(message) { //消息处理
-			session.MarkMessage(message, "") // 提交消息的消费确认信息
+			session.MarkMessage(message, "gemini parse success") // 提交消息的消费确认信息
 		} else {
 			fmt.Println("Message processing failed.") // 处理失败，不提交确认信息
 		}
@@ -105,7 +105,7 @@ func getKey() string {
 	if currentTime-count > 60 {
 		return key
 	} else {
-		time.Sleep(time.Second * 30)
+		time.Sleep(time.Second * 60)
 	}
 	return cache.GetKey()
 }
